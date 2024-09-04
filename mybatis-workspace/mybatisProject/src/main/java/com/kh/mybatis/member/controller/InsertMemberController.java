d16f91a7f57695f00b69310c79cf2513f99d2696
hailey-516 <leehi516@naver.com>
2024-09-04 16:04:29 +0900

    240904 framework study files

---
+++ mybatis-workspace/mybatisProject/src/main/java/com/kh/mybatis/member/controller/InsertMemberController.java
@@ -0,0 +1,54 @@
+package com.kh.mybatis.member.controller;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class InsertMemberController
+ */
+@WebServlet("/insert.me")
+public class InsertMemberController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public InsertMemberController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userId = request.getParameter("userId");s
+		String userPwd = request.getParameter("userPwd");
+		String userName = request.getParameter("userName");
+		String phone = request.getParameter("phone");
+		String email = request.getParameter("email");
+		String address = request.getParameter("address");
+		String[] interest = request.getParameterValues("interest");
+		
+		
+		
+		if ()
+		
+		
+		
+		
+	}
+
+}
